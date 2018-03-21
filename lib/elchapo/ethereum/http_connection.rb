01e9a52464aced382632a4fbed0c360ecaa0224d
DilumN <dilumnavanjana@gmail.com>
2018-03-21 16:01:15 +0800

    integrate REST::CLIENT to ethereum

--- lib/elchapo/ethereum/http_connection.rb
+++ lib/elchapo/ethereum/http_connection.rb
@@ -1,34 +1,25 @@
-require 'net/http'
 require 'json'
+require 'rest-client'
+
 module Ethereum
   class HttpConnection < Client
     attr_accessor :host, :port, :uri, :ssl
 
     def initialize(host)
       super()
-      uri = URI.parse(host)
-      raise ArgumentError unless ['http', 'https'].include? uri.scheme
-      @host = uri.host
-      @port = uri.port
 
-      @ssl = uri.scheme == 'https'
-      if ssl
-        @uri = URI("https://#{@host}:#{@port}")
-      else
-        @uri = URI("http://#{@host}:#{@port}")
-      end
+      @url = host
+      @header = {'Content-Type' => 'application/json'}
     end
 
     def send_single(payload)
-      http = ::Net::HTTP.new(@host, @port)
-      if @ssl
-        http.use_ssl = true
-      end
-      header = {'Content-Type' => 'application/json'}
-      request = ::Net::HTTP::Post.new(uri, header)
-      request.body = payload
-      response = http.request(request)
-      return response.body
+      resp = RestClient::Request.execute(
+        method: :post,
+        url: @url,
+        payload: payload,
+        headers: @header
+      )
+      return resp
     end
 
   end
